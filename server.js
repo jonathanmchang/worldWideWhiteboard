65d5dfa508321c6add7d45d8ae29e77ebfe4bd67
Jonathan Chang <jonathanmchang@gmail.com>
2017-10-05 16:52:48 -0400

    started git repo

---
+++ server.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+// app.listen() returns an http.Server object
+// http://expressjs.com/en/4x/api.html#app.listen
+const server = app.listen(1337, function () {
+    console.log(`Listening on http://localhost:${server.address().port}`);
+});
+
+// The webpack dev middleware builds and serves our bundle
+// directly from memory. This has the advantage of never serving
+// a partial bundle (if, for example, you reload in the middle of
+// a compilation).
+app.use(require('./webpack-middleware'))
+
+app.use(express.static(path.join(__dirname, 'browser')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
