5012e199fb441a57411ce6040f83b0def2f10e22
Jonathan Chang <jonathanmchang@gmail.com>
2017-10-05 23:05:28 -0400

    app and server are sending messages on draw

--- browser/app.js
+++ browser/app.js
@@ -2,10 +2,23 @@
 //   The default export, naming it draw,
 //   An export named `events`, calling it `whiteboard`.
 import whiteboard, {draw} from './whiteboard'
+const socket = io(window.location.origin)
+
+
 
 // Example of listening to draw events:
 //   (This logging will probably get really annoying):
-whiteboard.on('draw', console.log)
+whiteboard.on('draw', function(data) {
+    socket.emit('something', data)
+})
+socket.on('connect', function () {
+    console.log('I have made a persistent two-way connection to the server!')
+})
+
+socket.on('someoneDrew', function(data) {
+    console.log('someone drew something')
+    // draw(data);
+})
 
 // Example: Draw a single stroke.
-draw([0, 0], [250, 250], 'red', true)
+draw([0, 0], [250, 250], 'black', true)
--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const socketio = require('socket.io');
+
 
 // app.listen() returns an http.Server object
 // http://expressjs.com/en/4x/api.html#app.listen
 const server = app.listen(1337, function () {
     console.log(`Listening on http://localhost:${server.address().port}`);
 });
+
+const io = socketio(server)
+
+
+io.on('connection', function(socket) {
+    console.log('A new client has connected!');
+    console.log(socket.id)
+    socket.on('disconnect', function() { 
+        console.log('disconnected')
+    })
+    socket.on('something', function(data) {
+        console.log('i got something')
+        socket.broadcast.emit('someoneDrew', data)
+    })
+})
+
 
 // The webpack dev middleware builds and serves our bundle
 // directly from memory. This has the advantage of never serving
